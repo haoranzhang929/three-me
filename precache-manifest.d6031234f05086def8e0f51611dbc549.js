da8cef85c50cfe342396b5ac0774121fdba7af68
Haoran Zhang <haoranzhang@icloud.com>
2020-06-07 14:38:29 +0100

    Updates

---
+++ precache-manifest.d6031234f05086def8e0f51611dbc549.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "902346796f94a119172924f070bcd075",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d73a1b03a8a2264cf577",
+    "url": "/static/css/main.84c14fd6.chunk.css"
+  },
+  {
+    "revision": "e813426a37b131986ab4",
+    "url": "/static/js/2.56f6edca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.56f6edca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d73a1b03a8a2264cf577",
+    "url": "/static/js/main.f8ee12e1.chunk.js"
+  },
+  {
+    "revision": "d9ca220940d8a07109d4",
+    "url": "/static/js/runtime-main.e3d568e7.js"
+  }
+]);
